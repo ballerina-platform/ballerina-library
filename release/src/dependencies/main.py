ac72bfa572da4a71352b41d8c42e62b976a5c23f
Tharinda Dilshan Piyadasa <43146932+TharindaDilshan@users.noreply.github.com>
2020-11-03 11:20:09 +0530

    Update main.py

--- release/src/dependencies/main.py
+++ release/src/dependencies/main.py
@@ -2,15 +2,22 @@ import os
 import base64
 import requests
 import json
+import urllib.request
 
 
 url = 'https://api.github.com/repos/TharindaDilshan/module-ballerina-io/contents/build.gradle'
-req = requests.get(url)
-if req.status_code == requests.codes.ok:
-    req = req.json()  # the response is a JSON
+# req = requests.get(url)
+#if req.status_code == requests.codes.ok:
+    # req = req.json()  # the response is a JSON
     # req is now a dict with keys: name, encoding, url, size ...
     # and content. But it is encoded with base64.
-    content = base64.b64decode(req['content'].encode('ascii')).decode('ascii')
-    print(json.loads(content))
-else:
-    print('Content was not found.')
+    # content = base64.b64decode(req['content'].encode('ascii')).decode('ascii')
+    # print(json.loads(content))
+#else:
+    #print('Content was not found.')
+    
+file = urllib.request.urlopen(url)
+
+for line in file:
+	decoded_line = ase64.b64decode(line.encode('ascii')).decode('ascii')
+	print(decoded_line)
