421d412fafa192a0865f9bd6956be8b6a9c1bea4
Tharinda Dilshan Piyadasa <43146932+TharindaDilshan@users.noreply.github.com>
2020-11-03 10:40:14 +0530

    Update main.py

--- release/src/dependencies/main.py
+++ release/src/dependencies/main.py
@@ -1,3 +1,11 @@
 import os
+from github import Github
 
-print("Hello World", os.environ)
+# using username and password
+g = Github(os.environ["packageUser"], os.environ["packagePAT"])
+
+for repo in g.get_user().get_repos():
+    print(repo.name)
+    repo.edit(has_wiki=False)
+    # to see all the available attributes and methods
+    print(dir(repo))
