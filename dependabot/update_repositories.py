8d0770c7846eb921915f98600bb8ed0f2aae7d0b
Thisaru Guruge <thisaru.guruge@gmail.com>
2021-02-23 09:53:11 +0530

    Update dependency update PR title

--- dependabot/update_repositories.py
+++ dependabot/update_repositories.py
@@ -21,7 +21,7 @@ MASTER_BRANCH = "master"
 MAIN_BRANCH = "main"
 
 DEPENDABOT_BRANCH_NAME = "automated/stdlib_version_update"
-PULL_REQUEST_TITLE = "[Automated] Bump stdlib module versions"
+PULL_REQUEST_TITLE = "[Automated] Dependency Update"
 PULL_REQUEST_BODY = "$subject"
 
 PROPERTIES_FILE = "gradle.properties"
