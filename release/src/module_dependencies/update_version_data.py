b36e0fb62d1966827cb66f94f9f24e3c93d6e5c8
niveathika <niveathika@gmail.com>
2021-05-05 15:36:03 +0530

    Authenticate github api call

--- release/src/module_dependencies/update_version_data.py
+++ release/src/module_dependencies/update_version_data.py
@@ -4,6 +4,7 @@ import re
 import networkx as nx
 import sys
 import time
+import os
 from retry import retry
 
 HTTP_REQUEST_RETRIES = 3
@@ -14,6 +15,8 @@ BALLERINA_ORG_NAME = "ballerina-platform"
 BALLERINA_ORG_URL = "https://github.com/ballerina-platform/"
 GITHUB_BADGE_URL = "https://img.shields.io/github/"
 CODECOV_BADGE_URL = "https://codecov.io/gh/"
+
+ballerina_bot_token = os.environ["packagePAT"]
 
 def main():
     print('Running main.py')
@@ -41,34 +44,38 @@ def sort_module_name_list():
         sys.exit()
 
     name_list['modules'].sort(key=lambda x: x.split('-')[-1])
-    
+
     try:
         with open('./release/resources/module_list.json', 'w') as json_file:
-            json_file.seek(0) 
+            json_file.seek(0)
             json.dump(name_list, json_file, indent=4)
             json_file.truncate()
     except:
         print('Failed to write to file module_list.json')
         sys.exit()
-        
-    return name_list['modules'] 
+
+    return name_list['modules']
 
 # Returns the file in the given url
 # Retry decorator will retry the function 3 times, doubling the backoff delay if URLError is raised 
 @retry(
-    urllib.error.URLError, 
-    tries=HTTP_REQUEST_RETRIES, 
-    delay=HTTP_REQUEST_DELAY_IN_SECONDS, 
+    urllib.error.URLError,
+    tries=HTTP_REQUEST_RETRIES,
+    delay=HTTP_REQUEST_DELAY_IN_SECONDS,
     backoff=HTTP_REQUEST_DELAY_MULTIPLIER
 )
 def url_open_with_retry(url):
-    return urllib.request.urlopen(url)
+    request = urllib.request.Request(url)
+    request.add_header('Accept', 'application/vnd.github.v3+json')
+    request.add_header('Authorization', 'Bearer ' + ballerina_bot_token)
+
+    return urllib.request.urlopen(request)
 
 # Gets dependencies of ballerina standard library module from build.gradle file in module repository
 # returns: list of dependencies
 def get_dependencies(module_name):
     try:
-        data = url_open_with_retry("https://raw.githubusercontent.com/ballerina-platform/" 
+        data = url_open_with_retry("https://raw.githubusercontent.com/ballerina-platform/"
                                     + module_name + "/master/build.gradle")
     except:
         print('Failed to read build.gradle file of ' + module_name)
@@ -90,7 +97,7 @@ def get_dependencies(module_name):
 # returns: current version of the module
 def get_version(module_name):
     try:
-        data = url_open_with_retry("https://raw.githubusercontent.com/ballerina-platform/" 
+        data = url_open_with_retry("https://raw.githubusercontent.com/ballerina-platform/"
                                     + module_name + "/master/gradle.properties")
     except:
         print('Failed to read gradle.properties file of ' + module_name)
@@ -105,7 +112,7 @@ def get_version(module_name):
     if version == '':
         print('Version not defined for ' + module_name)
 
-    return version 
+    return version
 
 # Gets the default branch of the standard library repository
 # returns: default branch name
@@ -167,7 +174,7 @@ def calculate_levels(module_name_list, module_details_json):
             successors = []
             for i in G.successors(node):
                 successors.append(i)
-            for successor in successors:        
+            for successor in successors:
                 remove_modules_in_intermediate_paths(G, node, successor, successors, module_details_json)
                 G.nodes[successor]['level'] = level
                 if successor not in temp:
@@ -184,7 +191,7 @@ def calculate_levels(module_name_list, module_details_json):
 def update_json_file(updated_json):
     try:
         with open('./release/resources/stdlib_modules.json', 'w') as json_file:
-            json_file.seek(0) 
+            json_file.seek(0)
             json.dump(updated_json, json_file, indent=4)
             json_file.truncate()
     except:
@@ -197,16 +204,16 @@ def initialize_module_details(module_name_list):
     module_details_json = {'modules':[]}
 
     for module_name in module_name_list:
-        version = get_version(module_name)		
-        default_branch = get_default_branch(module_name)			
+        version = get_version(module_name)
+        default_branch = get_default_branch(module_name)
         module_details_json['modules'].append({
-            'name': module_name, 
+            'name': module_name,
             'version':version,
             'level': 0,
             'default_branch': default_branch,
-            'release': True, 
+            'release': True,
             'dependents': [] })
-        
+
         time.sleep(10)
 
     return module_details_json
@@ -219,7 +226,7 @@ def get_immediate_dependents(module_name_list, module_details_json):
         for module in module_details_json['modules']:
             if module['name'] in dependencies:
                 module_details_json['modules'][module_details_json['modules'].index(module)]['dependents'].append(module_name)
-                    
+
     return module_details_json
 
 # Updates the stdlib dashboard in README.md
@@ -250,31 +257,31 @@ def update_stdlib_dashboard(module_details_json):
             level_column = module['level']
             current_level = module['level']
 
-        row = ("|" + str(level_column) + "|" + 
-        "[" + module['name'].split('-')[-1] + "](" + BALLERINA_ORG_URL + module['name'] + ")| " + 
+        row = ("|" + str(level_column) + "|" +
+        "[" + module['name'].split('-')[-1] + "](" + BALLERINA_ORG_URL + module['name'] + ")| " +
 
-        "[![GitHub Release](" + GITHUB_BADGE_URL + "release/" + BALLERINA_ORG_NAME + "/" + module['name'] + ".svg?label=)]" + 
-        "(" + BALLERINA_ORG_URL + module['name'] + "/releases)| " + 
+        "[![GitHub Release](" + GITHUB_BADGE_URL + "release/" + BALLERINA_ORG_NAME + "/" + module['name'] + ".svg?label=)]" +
+        "(" + BALLERINA_ORG_URL + module['name'] + "/releases)| " +
 
-        "[![Build](" + BALLERINA_ORG_URL + module['name'] + "/actions/workflows/build-timestamped-master.yml/badge.svg)]" + 
-        "(" + BALLERINA_ORG_URL + module['name'] + "/actions/workflows/build-timestamped-master.yml)| " + 
+        "[![Build](" + BALLERINA_ORG_URL + module['name'] + "/actions/workflows/build-timestamped-master.yml/badge.svg)]" +
+        "(" + BALLERINA_ORG_URL + module['name'] + "/actions/workflows/build-timestamped-master.yml)| " +
 
         "[![CodeCov](" + CODECOV_BADGE_URL + BALLERINA_ORG_NAME + "/" + module['name'] + "/branch/" + module['default_branch'] + "/graph/badge.svg)]" +
-        "(" + CODECOV_BADGE_URL + BALLERINA_ORG_NAME + "/" + module['name'] + ")| " + 
-        
+        "(" + CODECOV_BADGE_URL + BALLERINA_ORG_NAME + "/" + module['name'] + ")| " +
+
         "[![Bugs](" + GITHUB_BADGE_URL + "issues-search/" + BALLERINA_ORG_NAME + "/ballerina-standard-library?"
                + get_bug_query(module) + ")](" + get_bugs_link(module) + ")| " +
 
-        "[![GitHub pull-requests](" + GITHUB_BADGE_URL + "issues-pr" + "/" + BALLERINA_ORG_NAME + "/" + module['name'] + ".svg?label=)]" + 
+        "[![GitHub pull-requests](" + GITHUB_BADGE_URL + "issues-pr" + "/" + BALLERINA_ORG_NAME + "/" + module['name'] + ".svg?label=)]" +
         "(" + BALLERINA_ORG_URL + module['name'] + "/pulls)|\n")
-        
+
         updated_readme_file += row
 
         level_column = ''
 
     try:
         with open('./README.md', 'w') as README:
-            README.seek(0) 
+            README.seek(0)
             README.write(updated_readme_file)
             README.truncate()
     except:
